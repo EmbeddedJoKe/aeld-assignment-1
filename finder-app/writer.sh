97180ca48d3572444e99098afbfba3ca35388654
devjo <devjo@Ubuntu.myguest.virtualbox.org>
2026-09-14 18:43:17 -0400

    writer app

---
+++ finder-app/writer.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#Joseph Kunitsky - Assignment 1 writer-app
+
+#Accept two runtime arguments (directory and text string)
+#Validate the arguments and directory
+#Creates new file with name and path
+#overwrite existing file and create path if it doesn't exist
+#print statement if file could not be created
+
+writefile=$1
+writestr=$2
+
+if [ $# -lt 2 ]
+then
+	echo "Invalid - please enter two arguments"
+	exit 1
+fi
+
+writedir=$(dirname "$writefile")
+
+mkdir -p "$writedir"
+
+if ! echo "$writestr" > "$writefile"
+then
+	echo "Could not create file"
+	exit 1
+fi
+
+echo "the number of files are $numfiles and the number of matching lines are $nummatches"
+
